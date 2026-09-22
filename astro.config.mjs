af91bfdc243bed99c4b49a3c989a2c83cef9b88c
Vinícius <rezende.vinicius.c@gmail.com>
2026-09-22 20:17:32 -0300

    adiciona busca

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,14 @@ import { defineConfig } from 'astro/config';
 import remarkBreaks from 'remark-breaks';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import pagefind from 'astro-pagefind';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://balaustres.netlify.app',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), pagefind()],
 
 	markdown: {
 		remarkPlugins: [remarkBreaks, remarkMath],
